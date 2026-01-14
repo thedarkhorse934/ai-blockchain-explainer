4e6361b60e4b815099f39b0ca78de4616e016269
thedarkhorse934 <william.morrison1989@proton.me>
2026-01-14 09:58:29 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+from blockchain.fetch_transaction import fetch_transaction
+
+
 def main():
-    print("AI Blockchain Explainer starting...")
+    tx_hash = "PUT_A_SEPOLIA_TX_HASH_HERE"
+
+    tx = fetch_transaction(tx_hash)
+
+    print("Transaction fetched successfully")
+    print("------------------------------")
+    print(f"From: {tx['from']}")
+    print(f"To: {tx['to']}")
+    print(f"Value (wei): {tx['value']}")
+
 
 if __name__ == "__main__":
     main()
